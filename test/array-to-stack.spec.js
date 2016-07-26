783d753566af3c197aba2cabb80b9576b1eaf7fc
Stefan Lehmann <stefan.lehmann@hpi.de>
2016-07-26 09:43:57 +0200

    get basic development workflow running

---
+++ test/array-to-stack.spec.js
@@ -0,0 +1,23 @@
+import {arrayToStack} from '../src/array-to-stack.js';
+
+describe('Array to Stack.', () => {
+
+    it('should work (imports a webpacked modules)', () => {
+        let s1 = arrayToStack([1,2,3]);
+        expect(s1.top()).to.equal(3);
+        s1.pop();
+        expect(s1.top()).to.equal(2);
+        s1.pop();
+        expect(s1.top()).to.equal(1);
+        s1.pop();
+        expect(s1.top()).to.be.undefined;
+
+        let s2 = arrayToStack([17, 42]);
+        expect(s2.top()).to.equal(42);
+        s2.pop();
+        expect(s2.top()).to.equal(17);
+        s2.pop();
+        expect(s2.top()).to.be.undefined;
+    });
+
+});
